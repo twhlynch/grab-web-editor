d672695601b2bffc64c83dd51591882864acd243
Tom Lynch <twhlynch@gmail.com>
2022-10-27 01:03:21 +1100

    controls

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'https://unpkg.com/three@0.145.0/examples/jsm/controls/OrbitControls.js';
 import { DragControls } from 'https://unpkg.com/three@0.145.0/examples/jsm/controls/DragControls.js';
+import { TransformControls } from 'https://unpkg.com/three@0.145.0/examples/jsm/controls/TransformControls.js';
 
 $( document ).ready(function() {
     $('.objects > div').click(function() {
@@ -21,17 +22,19 @@ $( document ).ready(function() {
         $('.shape.selected').removeClass('selected');
         $(this).addClass('selected');
     });
-    $(document).keydown(function(e) {
-        if (e.which == 17) {
-            controls.mouseButtons = {LEFT: 0, MIDDLE: 1, RIGHT: 2}
-            drag.deactivate();
+    //
+    $(document).mousedown(function(e) {
+        if (e.button == 1) {
+            drag.enabled = false;
         }
     });
-    $(document).keyup(function(e) {
-        if (e.which == 17) {
-            controls.mouseButtons = {}
-            drag.activate();
+    $(document).mouseup(function(e) {
+        if (e.button == 1) {
+            drag.enabled = true;
         }
+    });
+
+    $(document).keydown(function(e) {
         if (e.which == 68) {
             let geometry = current.geometry;
             let material = current.material;
@@ -48,17 +51,26 @@ $( document ).ready(function() {
             scene.add( obj );
             objects.push( obj );
         }
+        if (e.which == 84) {
+            control.setMode( "translate" );
+        }
+        if (e.which == 82) {
+            control.setMode( "rotate" );
+        }
+        if (e.which == 83) {
+            control.setMode( "scale" );
+        }
     });
 });
 
-let camera, scene, renderer, controls, drag, current;
+let camera, scene, renderer, light, controls, control, drag, current;
 let objects = [];
 
 class Cube {
     constructor(img) {
         let geometry = new THREE.BoxGeometry( 1, 1, 1 );
         let texture = new THREE.TextureLoader().load( 'textures/'+img+'.png' );
-        let material = new THREE.MeshBasicMaterial( { map: texture } );
+        let material = new THREE.MeshPhongMaterial( { map: texture } );
         let cube = new THREE.Mesh( geometry, material );
         scene.add( cube );
         objects.push( cube );
@@ -107,12 +119,49 @@ function init() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera( 75, (window.innerWidth - 300) / window.innerHeight, 0.1, 1000 );
     camera.position.z = 5;
+    light = new THREE.AmbientLight(0xffffff)
+    scene.add(light)
     controls = new OrbitControls( camera, renderer.domElement );
-    controls.mouseButtons = {}
-    drag = new DragControls( objects, camera, renderer.domElement)
+    controls.addEventListener( 'change', render );
+    controls.mouseButtons = {LEFT: 2, MIDDLE: 1, RIGHT: 0}
+    control = new TransformControls( camera, renderer.domElement );
+    control.addEventListener( 'change', render );
+    control.addEventListener( 'dragging-changed', function ( event ) {
+        controls.enabled = ! event.value;
+    } );
+    new Cube('default');
+    // for ( var i = 0; i < objects.length; i ++ ) {
+    //     control.attach( objects[ i ] );
+    //     scene.add( control );
+    // }
 
-    drag.addEventListener( 'dragstart', function ( event ) { current = event.object; console.log(event.object); } );
+
+
+    drag = new DragControls( objects, camera, renderer.domElement)
+    // drag.enabled = false;
+    drag.addEventListener( 'dragstart', function ( event ) { 
+        controls.enabled = false;
+        current = event.object;
+        event.object.material.emmissive = '0xffffff';
+        control.attach( event.object );
+        scene.add( control );
+    } );
+    drag.addEventListener( 'dragend', function ( event ) { 
+        controls.enabled = true;
+        event.object.material.emmissive = '0x000000';
+    } );
+    window.addEventListener( 'resize', onWindowResize );
 }
+
+function onWindowResize() {
+    camera.aspect = (window.innerWidth - 300) / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth - 300, window.innerHeight );
+
+    render();
+}
+
 
 function render() {
     requestAnimationFrame( render );
